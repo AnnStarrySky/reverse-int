b9aff6210ba7dd5c41d67722649adaa675a1fbb5
AnnStarrySky <anuta7267@mail.ru>
2025-08-26 12:10:47 +0700

    add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const number = Math.abs(n);
+  const str = number.toString().split('').reverse().join('');
+  return +str;
 };
